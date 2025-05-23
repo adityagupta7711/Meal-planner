7d0396a96efc5ba652dbdd12d75b49b1bd52b4b9
Aditya Gupta <guptaaditya1231@gmail.com>
2025-05-23 16:03:16 +0530

    updating to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,16 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'dist',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        port: "",
-        pathname: "/**",
-        hostname: "img.clerk.com",
-      },
-    ],
-  },
+    images:{
+      remotePatterns:[
+        {
+          
+          protocol: "https",
+          port:"",
+          pathname:"/**",
+          hostname:"img.clerk.com",
+        },
+      ],
+    },
 };
-
 export default nextConfig;
